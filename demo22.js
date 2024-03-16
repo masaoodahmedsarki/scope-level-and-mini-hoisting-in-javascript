6e9864d3f7807d66abc7a3dec96ea7dfab026eef
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-16 12:24:50 +0500

    scope level and mini hosting in javascript

---
+++ demo22.js
@@ -0,0 +1,41 @@
+function One (){
+    const username = "Masood";
+
+    function Two(){
+      const website = "youtube";
+      console.log(username);
+
+    }
+
+    //console.log(website);
+    Two()
+}
+
+
+//One()
+
+if (true) {
+    const username = "Masood"
+    if (username === "Masood") {
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    //console.log(website);
+}
+
+//console.log(username)
+
+//*********************************Intersting************************************
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+ 
+
+//addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
